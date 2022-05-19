aedfb3829a757da2708a5e33f060159b4028bee9
yasir-choudhry <91131643+yasir-choudhry@users.noreply.github.com>
2022-05-19 11:32:50 +0100

    download xmls from NaPTAN website, no longer select folders

--- ExcelToXml.py
+++ ExcelToXml.py
@@ -2,6 +2,9 @@ import PySimpleGUI as PyGUI
 import time
 import pandas as pd
 import datetime
+import os
+import requests
+import shutil
 
 
 def text_from_xml(xml_fp):
@@ -47,6 +50,53 @@ def get_xl_df(spreadsheet_fp, sheet):
     return pd.read_excel(spreadsheet_fp, sheet_name=sheet)
 
 
+def download_xml_from_naptan(la_name: str, xml_name: str, down_dir="downloaded_xmls"):
+    """
+    Submit a post request and download a xml file from the NaPTAN beta website and save in the downloaded_xmls folder
+    :param la_name: properly formatted name of local authority as required by the website (from the drop down)
+    :param xml_name: the name that the xml file should be saved as
+    :param down_dir: directory where the files are to be saved
+    :return:
+    """
+    req_data = {
+        "selectedLasNames": la_name,
+        "fileTypeSelect": "xml"
+    }
+    response = requests.post('https://beta-naptan.dft.gov.uk/Download/MultipleLa', data=req_data)
+    data = response.content
+
+    with open(down_dir+"/"+xml_name, 'wb') as s:
+        s.write(data)
+
+
+def check_national_xmls(xmls_req: list, down_dir="downloaded_xmls"):
+    """
+    Check if all the required xml files are stored in the downloaded_xmls folder
+    :param xmls_req: list of strings of xmls file names required ie ['910.xml', '920.xml']
+    :param down_dir: directory where the files are
+    :return: True if all are there, otherwise true
+    """
+    try:
+        os.makedirs(down_dir)
+    except FileExistsError:
+        # directory already exists
+        pass
+    files_present = os.listdir(down_dir)
+    for xml_file in xmls_req:
+        if xml_file not in files_present:
+            return False
+    return True
+
+
+def delete_downloaded_xmls(down_dir="downloaded_xmls"):
+    """
+    delete all files saved in the downloaded_xmls folder
+    :return:
+    """
+    shutil.rmtree(down_dir)
+    os.makedirs(down_dir)
+
+
 # These are not xml items and therefore need to be added differently
 attribute_name_list = ['CreationDateTime', 'ModificationDateTime', 'Modification', 'RevisionNumber', 'Status']
 
@@ -59,17 +109,26 @@ def add_to_log(log_var, str_to_add):
     return log_var
 
 
+# the file names and local authority names of the cml files required
+xml_name_la_names = {"910.xml": "National - National Rail / Great Britain (910)",
+                     "920.xml": "National - National Air / Great Britain (920)",
+                     "930.xml": "National - National Ferry / Great Britain (930)",
+                     "940.xml": "National - National Tram / Great Britain (940)"}
+
+if not check_national_xmls(list(xml_name_la_names.keys())):
+    output_text_log = add_to_log(output_text_log, "Missing xmls found, downloading from NaPTAN website")
+    delete_downloaded_xmls()
+    for xml_name, la_name in xml_name_la_names.items():
+        download_xml_from_naptan(la_name, xml_name)
+        output_text_log = add_to_log(output_text_log, "Downloaded "+xml_name)
+
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
 # First the window layout in 2 columns
 file_list_column = [
     [
-        PyGUI.Text("Select main XML folder (where the 900 xmls are saved):"),
-        PyGUI.In(size=(25, 1), enable_events=False, key="-MAIN FOLDER-"),
-        PyGUI.FolderBrowse(),
-    ],
-    [
-        PyGUI.Text("Select templates folder (where the stop xml templates are saved):"),
-        PyGUI.In(size=(25, 1), enable_events=False, key="-TEMPLATE FOLDER-"),
-        PyGUI.FolderBrowse(),
+        PyGUI.Button("Refresh XML files (delete and re-download form NaPTAN website)")
     ],
     [
         PyGUI.Text("Select Excel file (which contains the request(s)):"),
@@ -78,7 +137,7 @@ file_list_column = [
     ],
     [
         PyGUI.Button("Import from excel to xml")
-    ],
+    ]
 ]
 
 
@@ -102,6 +161,21 @@ while True:
     event, values = window.read()
     if event == "Exit" or event == PyGUI.WIN_CLOSED:
         break
+    elif event == "Refresh XML files (delete and re-download form NaPTAN website)":
+        try:
+            delete_downloaded_xmls()
+            output_text_log = add_to_log(output_text_log, "Deleted all xmls")
+            window['OUTPUT'].update(value=output_text_log)
+            for xml_name, la_name in xml_name_la_names.items():
+                download_xml_from_naptan(la_name, xml_name)
+                output_text_log = add_to_log(output_text_log, "Downloaded " + xml_name)
+                window['OUTPUT'].update(value=output_text_log)
+        except Exception as e:
+            output_text_log = add_to_log(output_text_log, "unexpected error when refreshing xmls")
+            window['OUTPUT'].update(value=output_text_log)
+            print(e)
+            pass
+
     elif event == "Import from excel to xml":  # A spreadsheet was chosen
         try:
             window['OUTPUT'].update(value=output_text_log)
@@ -170,7 +244,7 @@ while True:
             time.sleep(0.5)
 
         except Exception as e:
-            output_text_log = add_to_log(output_text_log, "unexpected error")
+            output_text_log = add_to_log(output_text_log, "unexpected error when importing spreadsheet")
             window['OUTPUT'].update(value=output_text_log)
             print(e)
             pass
